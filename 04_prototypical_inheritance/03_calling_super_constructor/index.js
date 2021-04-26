4f70a70ffb69c582620cde6b1805d897eadff27d
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-26 16:29:31 +0200

    learnign how to set the super constructor

---
+++ 04_prototypical_inheritance/03_calling_super_constructor/index.js
@@ -0,0 +1,23 @@
+function Shape(color) {
+    this.color = color;
+}
+
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+}
+
+function Circle(radius, color) {
+    Shape.call(this, color);
+
+    this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.draw = function() {
+    console.log('draw');
+}
+
+const shape = new Shape('green');
+const circle = new Circle(1, 'red');
