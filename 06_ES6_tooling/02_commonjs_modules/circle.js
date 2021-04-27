9c72c57e93c17e1fc0c9833e9a14522be05ffc33
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-27 18:59:33 +0200

    CommonJS modules (Node)

---
+++ 06_ES6_tooling/02_commonjs_modules/circle.js
@@ -0,0 +1,17 @@
+// Implementation detail
+const _radius = new WeakMap();
+
+// Public Interface
+class Circle {
+    constructor(radius) {
+        _radius.set(this, radius);
+    }
+
+    draw() {
+        console.log('Circle with radius', _radius.get(this));
+    }
+}
+
+module.exports = {
+    Circle: Circle
+};
---
+++ 06_ES6_tooling/02_commonjs_modules/index.js
@@ -0,0 +1,5 @@
+const circle = require('./circle');
+
+const c = new circle.Circle(10);
+
+c.draw()
